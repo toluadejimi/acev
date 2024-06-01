8dace634a926fc830dec109670fccaf3b010c798
Jimmy <toluadejimi@gmail.com>
2024-06-01 19:59:19 +0100

    pushhhhhh

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -187,7 +187,34 @@
 <script>preset_change('preset-4');</script>
 <script>main_layout_change('vertical');</script>
 
+</style>
 
+<style>
+    .float{
+        position:fixed;
+        width:60px;
+        height:60px;
+        bottom:40px;
+        right:40px;
+        background-color: #060d46;
+        color:#FFF;
+        border-radius:50px;
+        text-align:center;
+        font-size:30px;
+        box-shadow: 2px 2px 3px #999;
+        z-index:100;
+    }
+
+    .my-float{
+        margin-top:16px;
+    }
+</style>
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a style="color: white" href="https://t.me/acesmsverify" class="float" target="_blank">
+    <i class="fa fa-comment my-float"></i>
+</a>
 
 
 </body>
