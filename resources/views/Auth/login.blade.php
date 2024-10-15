ba367210342c27ddaca5b52784b7dcd5554b1828
Jimmy <toluadejimi@gmail.com>
2024-10-15 10:17:42 +0100

    pushhhhhh

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -7,29 +7,37 @@
 
 
             <div class="d-flex justify-content-center mt-5 my-5">
+
+
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                @if (session()->has('message'))
+                    <div class="alert alert-success">
+                        {{ session()->get('message') }}
+                    </div>
+                @endif
+                @if (session()->has('error'))
+                    <div class="alert alert-danger">
+                        {{ session()->get('error') }}
+                    </div>
+                @endif
+
+
+
                 <h4>Hi, Welcome Back! 👋</h4>
             </div>
 
 
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            @if (session()->has('message'))
-                <div class="alert alert-success">
-                    {{ session()->get('message') }}
-                </div>
-            @endif
-            @if (session()->has('error'))
-                <div class="alert alert-danger">
-                    {{ session()->get('error') }}
-                </div>
-            @endif
+
+
+
 
 
             <div class="d-flex justify-content-center mt-4">
