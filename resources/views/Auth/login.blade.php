3eb6283db440938e399a6a6be23ba1a804e4f66f
Jimmy <toluadejimi@gmail.com>
2024-10-19 04:26:56 +0100

    pushhhhhh

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -110,7 +110,7 @@
 
 
                     <div class="d-grid mt-4">
-                        <button type="submit" style="background: rgba(23, 69, 132, 1); border: 0px; border-radius: 2px"
+                        <button type="submit" style="background: rgba(23, 69, 132, 1); border: 0px; border-radius: 2px" disabled
                                 class="btn btn-primary">Login
                         </button>
                     </div>
