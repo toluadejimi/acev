3881b109689da7699c2d117b5badaddf32068004
Jimmy <toluadejimi@gmail.com>
2024-12-22 23:41:15 +0100

    nnn

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -289,8 +289,7 @@
                                                                 class="btn btn-warning btn-sm">Pending</span>
                                                             <a href="delete-order?id={{  $data->id }}&delete=1"
                                                                style="background: rgb(168, 0, 14); border:0px; font-size: 10px"
-                                                               onclick="hideButtondelete{{$data->id}}(this)"
-                                                               class="btn btn-warning btn-sm">Delete</span>
+                                                               class="btn btn-warning btn-sm hideButton">Delete</span>
 
                                                                 @else
                                                                     <span style="font-size: 10px;"
@@ -298,16 +297,14 @@
                                                         @endif
 
 
-                                                                <script>
-                                                                    function hideButtondelete{{$data->id}}(link) {
-                                                                        // Hide the clicked link
-                                                                        link.style.display = 'none';
-
-                                                                        setTimeout(function () {
-                                                                            link.style.display = 'inline'; // or 'block' depending on your layout
-                                                                        }, 10); // 5 seconds
-                                                                    }
-                                                                </script>
+                                                        <script>
+                                                            const buttons = document.querySelectorAll('.hideButton');
+                                                            buttons.forEach(button => {
+                                                                button.addEventListener('click', function() {
+                                                                    this.style.display = 'none';  
+                                                                });
+                                                            });
+                                                        </script>
 
                                                     </td>
                                                     <td id="datetime{{$data->id}}"
