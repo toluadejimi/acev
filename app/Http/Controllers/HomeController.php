b3a4bd29f17808857b457b460ec2ffda55f79656
Jimmy <toluadejimi@gmail.com>
2025-07-10 13:35:07 +0100

    pushhhhhh

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1205,7 +1205,7 @@ class HomeController extends Controller
         $ip = $request->ip();
         //$fund = $request->fund;
 
-        if($ip != $ipb ?? $ipa){
+        if($ip != $ipb || $ip != $ipb){
 
                 return response()->json([
                     'status' => false,
