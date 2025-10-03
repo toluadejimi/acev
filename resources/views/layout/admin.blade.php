fdcee5835f3833c70f9eac9b2dab6abf2da508e5
toluadejimi <toluadejimi@gmail.com>
2025-10-03 08:17:12 +0100

    pushhhhhh

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BoostVerify - Admin Dashboard</title>
+    <title>AceSMSVerify - Admin Dashboard</title>
     <link rel="stylesheet" href="{{url('')}}/public/assets/styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
