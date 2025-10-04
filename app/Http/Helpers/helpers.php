963601581fa70b5772b19fac8658954988295de9
toluadejimi <toluadejimi@gmail.com>
2025-10-04 22:21:08 +0100

    pushhhhhh

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -332,6 +332,10 @@ function create_order($service, $price, $cost, $service_name, $gcost, $area_code
         $result = $var ?? null;
 
 
+        Log::info("Diasy Response ====>>>". json_encode($result)."Data ===> $cost_new");
+
+
+
         if (strstr($result, "ACCESS_NUMBER") !== false) {
 
 
